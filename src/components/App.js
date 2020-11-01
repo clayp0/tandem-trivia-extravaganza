53688fbd3bd571b46ce6a3da24984727fd2d3e20
clayp0 <c.anders.christenson@gmail.com>
2020-11-01 14:38:11 -0600

    (add) user now chooses to start a round

--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,27 @@ const questions = require('../assets/Apprentice_TandemFor400_Data.json')
 
 
 const App = () => {
-  const [score, setScore] = useState(0)
+  const [score, setScore] = useState(0);
+  const [start, setStart] = useState(false);
   return (
     <div>
       <h1>BEHOLD! QUESTIONS!</h1>
-      <h2>You currently have {score} points</h2>
-      <QuestionList
-      questions={questions}
-      setScore={setScore}
-      score={score}
-      />
+      {!start && (
+        <div>
+          <h2>Would you care to start a round?</h2>
+          <button onClick={() => setStart(true)}>Yes please!</button>
+        </div>
+      )}
+      {start && (
+        <div>
+          <h2>You currently have {score} points</h2>
+          <QuestionList
+            questions={questions}
+            setScore={setScore}
+            score={score}
+          />
+        </div>
+      )}
     </div>
   );
 };
